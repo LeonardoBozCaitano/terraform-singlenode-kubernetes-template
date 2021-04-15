7717cc90cbf5e6278d8c881e028ca11ab85f909a
Leonardo Boz Caitano <leonardoboz@gmail.com>
2021-04-15 12:08:17 -0300

    Start commands correction

--- start_k8s.sh
+++ start_k8s.sh
@@ -4,6 +4,16 @@ apt update -y
 snap install microk8s --classic --channel=1.21/stable
 microk8s status --wait-ready
 
+microk8s.enable dns
+microk8s.enable dashboard
+microk8s.enable ingress
+microk8s.enable helm3
+snap alias microk8s.helm3 helm
+snap alias microk8s.kubectl kubectl
+
+kubectl proxy --accept-hosts=.* --address=0.0.0.0 &
+kubectl config view --raw >~/.kube/config
+
 mkdir /home/k8s && \
   groupadd -r k8s && \
   useradd -s /bin/bash -d /home/k8s -r -g k8s k8s && \
@@ -11,18 +21,8 @@ mkdir /home/k8s && \
 
 cd home/k8s
 git clone https://github.com/LeonardoBozCaitano/terraform-singlenode-kubernetes-template.git
-cd terraform-singlenode-kubernetes-template/helm-config
+cd terraform-singlenode-kubernetes-template/helm-charts
 
-microk8s.enable dns
-microk8s.enable dashboard
-microk8s.enable ingress
-microk8s.enable fluentd
-microk8s.enable helm3
-snap alias microk8s.helm3 helm
-snap alias microk8s.kubectl kubectl
-
-kubectl proxy --accept-hosts=.* --address=0.0.0.0 &
-kubectl config view --raw >~/.kube/config
 kubectl apply -f persistence-volume/values.yaml
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
