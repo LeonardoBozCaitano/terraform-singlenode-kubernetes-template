13653635c6114d2250f18387e36915e12772784d
Leonardo Boz Caitano <leonardoboz@gmail.com>
2021-04-17 17:58:18 -0300

    Changes on project architect and jenkins remove

--- start_k8s.sh
+++ start_k8s.sh
@@ -14,6 +14,7 @@ snap alias microk8s.kubectl kubectl
 
 kubectl proxy --accept-hosts=.* --address=0.0.0.0 &
 kubectl config view --raw >~/.kube/config
+helm init
 
 mkdir /home/k8s && \
   groupadd -r k8s && \
@@ -21,12 +22,15 @@ mkdir /home/k8s && \
   chown k8s:k8s /home/k8s
 
 cd home/k8s
-
 git clone https://github.com/LeonardoBozCaitano/terraform-singlenode-kubernetes-template.git
-cd terraform-singlenode-kubernetes-template/helm-charts
+cd terraform-singlenode-kubernetes-template/charts
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install --values=mongodb/values.yaml bitnami/mongodb --generate-name
-helm install --values=jenkins/values.yaml bitnami/jenkins --generate-name
+
+helm install mongodb bitnami/mongodb --values=mongodb/values.yaml
+
+kubectl create namespace jenkins
+# helm install jenkins bitnami/jenkins --values=jenkins/values.yaml --namespace jenkins
+# kubectl --namespace jenkins port-forward jenkins-6fc5b56f5d-96hgn 8080:8080
 
 mkdir ./ready
